be030d10de4cb2b13feb9e245c3a1ab0985b30a5
nunov18 <nunov18@gmail.com>
2017-04-11 22:11:02 +0100

    Ny times API

--- js/script.js
+++ js/script.js
@@ -23,6 +23,19 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetviewUrl +'">');
 
+    // Your NYTimes AJAX request goes HERE
+
+    var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '?sort=newest&api-key=6f6fc6b6e8a8445686b80e7809caed8e'
+    $.getJSON(nytimesUrl, function (data) {
+      $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+      articles = data.response.docs;
+      for (var i = 0; i < articles.lenght; i++) {
+        var article = articles[i];
+        $nytElem.append('<li class="article"' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+      };
+    });
+
+
     return false;
 };
 
