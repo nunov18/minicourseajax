03e102738afe852cccc3b0da88356984a438b134
nunov18 <nunov18@gmail.com>
2017-04-12 00:26:17 +0100

    Ny times

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-function loadData() {
+/* function loadData() {
 
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
@@ -26,20 +26,69 @@ function loadData() {
     // Your NYTimes AJAX request goes HERE
 
     var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '?sort=newest&api-key=6f6fc6b6e8a8445686b80e7809caed8e';
-    $.getJSON(nytimesUrl, function (data) {
-      console.log(data);
-      $nytHeaderElem.text('New York Times Articles About '+cityStr);
+    $.getJSON(nytimesUrl, function(data){
+      $nytHeaderElem.text('New York Times Articles About ' + cityStr);
 
       articles = data.response.docs;
       for (var i = 0; i < articles.lenght; i++) {
         var article = articles[i];
-        $nytElem.append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+        $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>'+'<p>' + article.snippet + '</p>'+'</li>');
       };
 
-    });
+    })
 
 
     return false;
 };
+
+$('#form-container').submit(loadData);
+
+
+//6f6fc6b6e8a8445686b80e7809caed8e
+
+*/
+
+function loadData() {
+
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    // load streetview
+
+    var $street = $("#street").val();
+    var $city = $("#city").val();
+    var $address = $street + ', ' + $city;
+
+    $greeting.text("So you want to live at " + $address + '?');
+
+    var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + $address + '';
+    $body.append('<img class="bgimg" src="' + streetviewURL + '">');
+
+    // Your NYTimes AJAX request goes here
+
+    var nyURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + $city +
+    "&sort=newest&api-key=6f6fc6b6e8a8445686b80e7809caed8e";
+                $.getJSON(nyURL, function(data) {
+                    $nytHeaderElem.text('Articles About ' + $city);
+
+                    articles = data.response.docs;
+                    for (var i = 0; i < articles.length; i++) {
+                        var article = articles[i];
+                        $nytElem.append('<li class="article">' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+
+
+                    };
+                       return false;
+
+                })
+}
+
 
 $('#form-container').submit(loadData);
