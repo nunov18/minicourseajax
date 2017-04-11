f9c6ee50b9fc6883e5f2045273057c3d56edd576
nunov18 <nunov18@gmail.com>
2017-04-11 23:13:48 +0100

    NYT API

--- js/script.js
+++ js/script.js
@@ -25,14 +25,17 @@ function loadData() {
 
     // Your NYTimes AJAX request goes HERE
 
-    var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '?sort=newest&api-key=6f6fc6b6e8a8445686b80e7809caed8e'
+    var nytimesUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '?sort=newest&api-key=6f6fc6b6e8a8445686b80e7809caed8e';
     $.getJSON(nytimesUrl, function (data) {
-      $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+      console.log(data);
+      $nytHeaderElem.text('New York Times Articles About '+cityStr);
+
       articles = data.response.docs;
       for (var i = 0; i < articles.lenght; i++) {
         var article = articles[i];
-        $nytElem.append('<li class="article"' + '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+        $nytElem.append('<li class="article">' + '<a href=" '+article.web_url+'">'+article.headline.main+'</a>'+'<p>' + article.snippet + '</p>'+'</li>');
       };
+
     });
 
 
